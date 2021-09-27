d04255c51dcf1b39f1e45e7637919ebd83a65bf1
lucas malatchoumy <malatchoumylucas@gmail.com>
2021-09-27 16:45:28 +0200

    feat(footer): add footer in index.php

---
+++ includes/footer.php
@@ -0,0 +1,27 @@
+<footer>
+  <div class=" footer2 navbar-dark bg-dark">
+    <div class=" footer-copyright text-center py-3 text-success">© 2020 Copyright:
+      <a href="https://mdbootstrap.com/"> Uniblog</a>
+    </div>
+    <ul>
+      <li class="footer1">
+        <a class="mr-5" href="#">Accueil</a>
+        <a class="mr-5" href="#">Blog</a>
+        <a class="mr-5" href="#">Inscription</a>
+        <a class="mr-5" href="#">Connexion</a>
+      </li>
+    </ul>
+  </div>
+  <style>
+      .footer2 {
+        display: center;
+        margin-top: 50px;
+        padding-bottom: 10px;
+        padding-top: 10px;
+      }
+      .footer1 {
+        display: flex;
+        justify-content: center;
+      }
+  </style>
+</footer>
