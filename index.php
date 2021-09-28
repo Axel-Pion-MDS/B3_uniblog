d4394fc9b788ed09e69da7f4137eaf8530488f2b
lucas malatchoumy <malatchoumylucas@gmail.com>
2021-09-28 10:54:18 +0200

    fix(index): include footer in index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  require('config/config.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <?php include('includes/head.php') ?>
+  <title>Uniblog</title>
+</head>
+<body>
+  <?php include('includes/header.php') ?>
+  <h1>Uniblog</h1>
+  <?php include('includes/footer.php') ?>
+</body>
+</html>
