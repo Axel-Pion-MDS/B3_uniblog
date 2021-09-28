38eab1061d302c4de71ddc1dbdc241e771699682
Rémi Rubis <remi.rubis@yahoo.com>
2021-09-28 23:53:08 +0200

    fix(register): Solve problems with check operations

--- core/registration.php
+++ core/registration.php
@@ -1,22 +1,35 @@
 <?php
-  require("../config/config.php");
-  /* Check Login form submitted */
-  if ( isset( $_POST['Submit'] ) ) {
-    /* Check if some entries are empty*/ 
-    if (empty($_POST['Username']) || empty($_POST['Password'])){
-      header("Location: ../register.php");
-      exit;
-    }
-    /* assign Usernames and Passwords to variables*/
-    $username = isset($_POST['Username']) ? $_POST['Username'] : '';
-    $password = isset($_POST['Password']) ? $_POST['Password'] : '';
+require("../config/config.php");
 
-    $user = new User();
-    $user->setUsername("$username");
-    $user->setPassword(crypt_password($password));
-    $user->setCreatedAt(date("m-d-Y"));
-    $user->setUpdatedAt(date("m-d-Y"));
-    add_content($user,users);
-  }
-  header("Location: ../index.php");
+// Check if something was sent in post method
+if (empty($_POST)) {
+  header("Location: ../register.php");
   exit;
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Check if entries are empty 
+if (empty($username) || empty($password)) {
+  header("Location: ../register.php");
+  exit;
+}
+
+$date = date_create();
+
+$created_at = date_timestamp_get($date);
+$updated_at = date_timestamp_get($date);
+
+$user = new User();
+$user->setUsername($username);
+$user->setPassword(crypt_password($password));
+$user->setCreatedAt($created_at);
+$user->setUpdatedAt($updated_at);
+
+add_content($user,"users");
+
+$_SESSION["user"] = $user->getUsername();
+
+header("Location: ../index.php");
+exit;
