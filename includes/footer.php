e667238ac667563b10570a87135fdc7062565176
lucas malatchoumy <malatchoumylucas@gmail.com>
2021-09-28 09:59:42 +0200

    fix(footer): add link for redirect user on Homepage and update syle

--- includes/footer.php
+++ includes/footer.php
@@ -1,27 +1,15 @@
 <footer>
-  <div class=" footer2 navbar-dark bg-dark">
+  <div class="d-center mt-5 pt-2 pb-2 navbar-dark bg-dark">
     <div class=" footer-copyright text-center py-3 text-success">© 2020 Copyright:
       <a href="https://mdbootstrap.com/"> Uniblog</a>
     </div>
-    <ul>
-      <li class="footer1">
-        <a class="mr-5" href="#">Accueil</a>
-        <a class="mr-5" href="#">Blog</a>
-        <a class="mr-5" href="#">Inscription</a>
-        <a class="mr-5" href="#">Connexion</a>
-      </li>
-    </ul>
-  </div>
-  <style>
-      .footer2 {
-        display: center;
-        margin-top: 50px;
-        padding-bottom: 10px;
-        padding-top: 10px;
-      }
-      .footer1 {
-        display: flex;
-        justify-content: center;
-      }
-  </style>
+      <ul>
+        <li class="d-flex justify-content-center">
+          <a class="mr-3" href="../index.php">Accueil</a>
+          <a class="mr-3" href="#">Blog</a>
+          <a class="mr-3" href="#">Inscription</a>
+          <a class="mr-3" href="#">Connexion</a>
+        </li>
+      </ul>
+    </div>
 </footer>
