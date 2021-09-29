33b7065791ed3fcbd74b5f3d6e0e899df07a37dc
Rémi Rubis <remi.rubis@yahoo.com>
2021-09-29 10:52:19 +0200

    fix(front): Some corrections and optimizations

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 </head>
 <body>
   <?php include('includes/header.php') ?>
-  <div class="card-body ml-4">
+  <div class="card-body mx-4">
     <h2 class="mb-4">Overwiew of all articles</h2>
     <?php foreach ($posts as $key => $post) { ?>
-      <article class="row mb-2 border rounded">
+      <article class="row mb-3 rounded shadow">
         <div class="col-3 p-0 w-100"><a href="post.php?id=<?= $key ?>"><img src="<?= $post['img'] ?>" class="img-fluid rounded"></a>
         </div>
-        <div class="col-sm-9 p-3">
+        <div class="col-sm-9 p-4">
           <h3 class="title"><a href="post.php?id=<?= $key ?>"><?= $post['title'] ?></a></h3>
           <p class="text-muted">By <a href="#"><?= $post['author'] ?></a> | <?= date('d M Y H:i', $post['created_at']) ?></p>
           <p><?= substr($post['content'],0,300) . '...' ?></p>
--- post.php
+++ post.php
@@ -20,16 +20,59 @@
 <head>
   <?php include('includes/head.php') ?>
   <title>Article | Uniblog</title>
+  <style>
+    figure {
+      position: relative;
+      height: 250px;
+      width: 100%;
+      overflow: hidden;
+    }
+    img {
+      position: absolute;
+      top: 50%;
+      width: 100%;
+      transform: translateY(-50%);
+    }
+    figure:after {
+      position: absolute;
+      content: '';
+      z-index: 1;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, .3);
+    }
+    .img {
+      position: relative;
+    }
+    .back {
+      position: absolute;
+      z-index: 3;
+      top: 20px;
+      left: 45px;   
+    }
+    .post-title {
+      position: absolute;
+      z-index: 3;
+      bottom: 10px;
+      left: 45px
+    }
+  </style>
 </head>
 <body>
   <?php include('includes/header.php') ?>
-  <a class="ml-4 btn btn-outline-danger mb-3 ml-1" href="index.php">Back home</a>
-  <a class="ml-4 btn btn-outline-warning mb-3 ml-1" href="postUpdate.php?id=<?= $_GET['id'] ?>">Update post</a>
-  <article class="flex-column p-3 mx-4 border border-dark rounded mb-4">
-    <figure>
+  <div class="img">
+    <div class="back">
+      <a class="btn btn-primary" href="index.php">Back to home</a>
+      <a class="btn btn-warning" href="postUpdate.php?id=<?= $_GET['id'] ?>">Update post</a>
+    </div>
+    <figure class="mb-0">
       <img src="<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
     </figure>
-    <h2><?= $post['title'] ?></h2>
+    <h1 class="post-title text-white"><?= $post['title'] ?></h1>
+  </div>
+  <article class="px-4 py-3 mx-4 shadow rounded-bottom">
     <p class="text-muted">By <a href="#"><?= $post['author'] ?></a> | <?= date('d M Y H:i', $post['created_at']) ?></p>
     <p><?= $post['content'] ?></p>
   </article>
